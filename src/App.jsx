f1858d9ef30946dd1d780418f01b96e733513659
asseniliev <assen.s.iliev@gmail.com>
2023-05-09 22:27:32 +0200

    create the header banner

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+
+/**
+ * Challenge: Build the Header component
+ */
+export default function App() {
+  return (
+    <div className="main-container">
+      <Header />
+    </div>
+
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div className="header">
+      <img src="src/images/troll-face.png" alt="troll face" className="header-image" />
+      <p className="header-title">Meme Generator</p>
+      <p className="header-lesson">React Course - Project 3</p>
+    </div>
+  )
+}
