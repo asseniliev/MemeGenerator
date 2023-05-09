fcbd89b1aa369595ffd42bb74d6fa547d9e81c20
asseniliev <assen.s.iliev@gmail.com>
2023-05-10 00:32:55 +0200

    format the meme button

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import memeData from '../memeData';
 
 export default function Meme() {
+
+  function handleOnClick(array) {
+    const index = Math.floor(Math.random() * array.length);
+    console.log(array[index].url);
+  }
+
+
   return (
     <div className='meme-container'>
       <div className='meme-input-container'>
@@ -13,7 +21,7 @@ export default function Meme() {
           placeholder='and take my money'
         />
       </div>
-      <button className="meme-button">Get a new meme image</button>
-    </div>
+      <button className="meme-button" onClick={() => handleOnClick(memeData.data.memes)}>Get a new meme image &#129347;</button>
+    </div >
   )
 }
